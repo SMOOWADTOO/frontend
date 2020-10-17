1d8cd01b28256487a7450d36069f71d5c96e5a28
Emmanuel Rayendra <emmanuelr.2018@sis.smu.edu.sg>
2020-10-17 17:03:26 +0800

    Created: Static page template

---
+++ casafair/components/templates/StaticPageHeader.vue
@@ -0,0 +1,25 @@
+<template>
+    <section>
+        <section class="hero">
+            <div class="hero-body">
+                <div class="container">
+                <h1 class="title">
+                    {{title}}
+                </h1>
+                <h2 class="subtitle">
+                    {{description}}
+                </h2>
+                </div>
+            </div>
+        </section>
+    </section>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+        description: String,
+    }
+}
+</script>
