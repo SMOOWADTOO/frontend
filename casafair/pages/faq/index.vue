db98ea28d7a9c9e73a69499f98eb4bd0cb406ada
Emmanuel Rayendra <emmanuelr.2018@sis.smu.edu.sg>
2020-10-17 17:03:42 +0800

    Created: FAQ Page

---
+++ casafair/pages/faq/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <section>
+        <StaticPageHeader :title="pageName" :description="pageDescription"/>
+    </section>
+</template>
+
+<script>
+export default {
+    head () {
+        return {
+            title: this.appName + " | " + this.pageName,
+            meta: [
+                // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+                { hid: 'description', name: 'description', content: this.pageDescription }
+            ]
+        }
+    },
+    data() {
+        return {
+            pageName: "FAQs",
+            pageDescription: "Answering all frequently asked questions",
+        }
+    }
+}
+</script>
