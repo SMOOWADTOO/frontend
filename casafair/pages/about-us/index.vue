694a6c995a15563b824e24ade56a9e766acb51ca
youxuanpeng <54617188+youxuanpeng@users.noreply.github.com>
2020-10-21 03:00:50 +0800

    about-us index

--- casafair/pages/about-us/index.vue
+++ casafair/pages/about-us/index.vue
@@ -1,7 +1,77 @@
 <template>
     <section>
         <StaticPageHeader :heroClass="'has-text-centered'" :title="pageName" :description="pageDescription"/>
+                <div class="columns is-3 my-5 is-vcentered is-centered">
+            <div class="column is-5">
+                <div class="title">About Us</div>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum
+                </p>
+
+            </div>
+            <div class="column is-1"></div>
+            <div class="column is-5">
+                <figure class="image is-60x60">
+                    <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
+                </figure>
+            </div>
+            
+
+
+        </div>
+
+        <!-- </div> -->
+
+        <div class="columns is-8 my-5 is-vcentered is-centered">
+            <div class="column is-5">
+                <figure class="image is-60x60">
+                    <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
+                </figure>
+            </div>
+            <div class="column is-1"></div>
+
+            <div class="column is-5">
+                <div class="title">Our Vision</div>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum
+                </p>
+
+            </div>
+
+
+        </div>
+
+        <div class="columns is-3 my-5 is-vcentered is-centered">
+            <div class="column is-5">
+                <div class="title">Our Mission</div>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum
+                </p>
+
+            </div>
+            <div class="column is-1"></div>
+            <div class="column is-5">
+                <figure class="image is-60x60">
+                    <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
+                </figure>
+            </div>
+
+
+        </div>
     </section>
+
 </template>
 
 <script>
