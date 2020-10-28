0929648986c103cb266e46a8588ff2f4894bec9b
qijintay <qijintay@users.noreply.github.com>
2020-10-28 21:58:02 +0800

    Created "Edit Order" Page

---
+++ casafair/pages/edit-order/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="columns is-centered  is-vcentered">
+        <div class="column is-3">
+            <div class="title has-text-centered">Order ID: 12345678</div>
+            <figure class="image">
+                <img src="https://bulma.io/images/placeholders/128x128.png">
+                <p class="is-overlay mx-5 is-size-4-desktop is-size-3-touch" style="margin-top: 60%;">
+                    Product Name
+                    <br>
+                    <small class="is-size-6">Product Description</small>
+                </p>
+            </figure>
+        </div>
+        <div class="column is-1"></div>
+        <div class="column is-5">
+            <div class="title is-3">$12.96</div>
+            <div class="subtitle is-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum
+            </div>
+            <div class="buttons has-addons">
+                <button class="button is-medium">-</button>
+                <button class="button is-static is-medium">1</button>
+                <button class="button is-medium">+</button>
+            </div>
+            <button class="button is-medium is-fullwidth is-primary is-outlined">Edit Order</button>
+        </div>
+    </div>
+</template>
