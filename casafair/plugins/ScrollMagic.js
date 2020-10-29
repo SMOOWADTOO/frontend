3de260a4276da932c25dc6d1a85784b4f77fe98e
Emmanuel Rayendra <emmanuelr.2018@sis.smu.edu.sg>
2020-10-29 18:51:36 +0800

    Implemented: Scrolling animations using GSAP3

--- casafair/plugins/ScrollMagic.js
+++ casafair/plugins/ScrollMagic.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import * as ScrollMagic from 'scrollmagic';
 import { gsap } from 'gsap'
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 Object.defineProperty(Vue.prototype, '$scrollmagic', { value: ScrollMagic });
 Object.defineProperty(Vue.prototype, '$gsap', { value: gsap });
+Object.defineProperty(Vue.prototype, '$scrollTrigger', { value: ScrollTrigger });
