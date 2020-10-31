8d4cf6209e9e6f3251320fd53454fe7e587aeccc
youxuanpeng <54617188+youxuanpeng@users.noreply.github.com>
2020-10-31 23:07:01 +0800

    Edit homepage frontend

--- casafair/components/items/CaptionBox.vue
+++ casafair/components/items/CaptionBox.vue
@@ -1,51 +1,60 @@
 <template>
-    <div class="card">
+
+    <div class="card is-fullimage equal-height">
         <div class="card-image">
             <figure class="image is-4by3">
                 <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
             </figure>
-        </div>
-        <div class="card-content">
-            <div class="content">
-                <b-skeleton width="100%" :animated="true" v-if="this.loading"></b-skeleton>
-                <span v-else>{{profile.description}}</span>
+            <div class="card-stacked full-height">
+                <div class="card-content equal-height to-bottom">
+                    <div class="media-content">
+                        <p class="title is-4">{{shopOwner}}</p>
+                    </div>
+
+                    <div class="content">
+                        <b-skeleton width="100%" :animated="true" v-if="this.loading"></b-skeleton>
+                        <span v-else>{{profile.description}}</span>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+
 </template>
 
 <script>
-export default {
-    props: {
-        description: String,
-    },
-    watch: {
-        description: function() {
-            this.fetchOwnerProfile()
-        }
-    },
-    data() {
-        return {
-            profile: {},
-            loading: true,
-        }
-    },
-    methods: {
-        fetchOwnerProfile() {
-            let r = this.$axios.get(this.USERAPI + "/profile/" + this.description.id).then((response) => {
-                this.loading = false
-                this.profile = response.data.user_profile
-            }).catch((error) => {
-                if (error.response != undefined) {
-                    var response = error.response.data
-                    this.toastAlert(response.message, "is-danger", 5000)
-                } else {
-                    this.toastAlert(error, "is-danger", 5000)
-                }
-            })
+    export default {
+        props: {
+            description: String,
         },
+        watch: {
+            description: function () {
+                this.fetchOwnerProfile()
+            }
+        },
+        data() {
+            return {
+                profile: {},
+                loading: true,
+            }
+        },
+        methods: {
+            fetchOwnerProfile() {
+                let r = this.$axios.get(this.USERAPI + "/profile/" + this.description.id).then((response) => {
+                    this.shopOwner = response.data.user_profile.firstName; 
+                    this.loading = false
+                    this.profile = response.data.user_profile
+                }).catch((error) => {
+                    if (error.response != undefined) {
+                        var response = error.response.data
+                        this.toastAlert(response.message, "is-danger", 5000)
+                    } else {
+                        this.toastAlert(error, "is-danger", 5000)
+                    }
+                })
+            },
+        }
     }
-}
 </script>
 
 <style scoped>
--- casafair/components/items/HorizontalCard.vue
+++ casafair/components/items/HorizontalCard.vue
@@ -1,24 +1,20 @@
 <template>
-    <div class="card" style="margin: 3rem 0;">
-        <div class="columns">
-            <div class="column">
-                <div class="card-image">
-                    <figure class="image is-4by3 has-background-grey-lighter">
-                    </figure>
-                </div>
-            </div>
-            <div class="column">
-                <div class="card-content">
-                    <div class="media">
-                        <div class="media-content">
-                            <p class="title">{{productData.productName}}</p>
-                            <p class="subtitle">{{productData.productDesc}}</p>
-                        </div>
-                    </div>
+    <div class="card is-horizontal">
+        <div class="card-image">
+            <figure class="image is-4by3 has-background-grey-lighter">
 
-                    <div class="content">
-                        <p>Price: ${{productData.unitPrice.toFixed(2)}}</p>
-                    </div>
+            </figure>
+        </div>
+        <div class="card-stacked">
+            <div class="card-content">
+                <div class="media-content">
+                    <p class="title is-4">{{productData.productName}}</p>
+                    <p class="subtitle is-6 content-text">{{productData.productDesc}}</p>
+                </div>
+
+                <div class="content">
+                    <br>
+                    Price: ${{productData.unitPrice.toFixed(2)}}
                 </div>
             </div>
         </div>
@@ -26,14 +22,14 @@
 </template>
 
 <script>
-export default {
-    props: {
-        productData: Object,
-    },
-    data() {
-        return {
-            animated: true
-        }
-    },
-}
+    export default {
+        props: {
+            productData: Object,
+        },
+        data() {
+            return {
+                animated: true
+            }
+        },
+    }
 </script>
