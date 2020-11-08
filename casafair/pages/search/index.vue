0b9df1ef618b31dd5016b46e558c63d37e8a1c8a
Vi <54704677+beanwillis@users.noreply.github.com>
2020-11-08 10:06:45 +0800

    Update Search page SearchBox component
    - Put it in div for some margin

--- casafair/pages/search/index.vue
+++ casafair/pages/search/index.vue
@@ -1,7 +1,10 @@
 <template>
   <section>
-    <SearchBox />
-    {{ search_type }}
+            <div class="columns mx-0">
+            <div class="column is-12">
+                <SearchBox/>
+            </div>
+        </div>
     <div
       class="columns is-multiline is-desktop"
       v-if="this.search_type === 'product'"
