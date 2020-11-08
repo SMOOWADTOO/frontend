1d818a908fbd863cf76103915b7e2cb883850af6
qijintay <qijintay@users.noreply.github.com>
2020-11-08 22:37:28 +0800

    Remove unwanted content on contact-us page

--- casafair/pages/contact-us/index.vue
+++ casafair/pages/contact-us/index.vue
@@ -53,63 +53,6 @@
             </div>
         </div>
 
-
-        <div class="columns my-5">
-            <div class="column is-6 is-offset-3">
-                <p class="title has-text-centered">Helpful topics</p>
-                <br>
-                <nav class="level my-5 px-5">
-                    <div class="level-item has-text-centered">
-                        <div>
-                            <p class="heading">Grievances</p>
-                            <b-icon icon="account" size="is-large" type="is-primary">
-                            </b-icon>
-                        </div>
-                    </div>
-                    <div class="level-item has-text-centered">
-                        <div>
-                            <p class="heading">Claims</p>
-                            <b-icon icon="account" size="is-large" type="is-primary">
-                            </b-icon>
-                        </div>
-                    </div>
-                    <div class="level-item has-text-centered">
-                        <div>
-                            <p class="heading">Feedback</p>
-                            <b-icon icon="account" size="is-large" type="is-primary">
-                            </b-icon>
-                        </div>
-                    </div>
-                </nav>
-            </div>
-        </div>
-        <hr>
-        <div class="columns my-5">
-            <div class="column is-6 is-offset-3">
-                <div class="columns is-multiline mx-3">
-                    <div class="column is-6">
-                        <b-icon icon="account" size="is-large" type="is-primary">
-                        </b-icon>
-                        <p>Lorem Ipsum ist ein einfacher Demo-Text für die Print- und Schriftindustrie.</p>
-                    </div>
-                    <div class="column is-6">
-                        <b-icon icon="account" size="is-large" type="is-primary">
-                        </b-icon>
-                        <p>Lorem Ipsum ist ein einfacher Demo-Text für die Print- und Schriftindustrie.</p>
-                    </div>
-                    <div class="column is-6">
-                        <b-icon icon="account" size="is-large" type="is-primary">
-                        </b-icon>
-                        <p>Lorem Ipsum ist ein einfacher Demo-Text für die Print- und Schriftindustrie.</p>
-                    </div>
-                    <div class="column is-6">
-                        <b-icon icon="account" size="is-large" type="is-primary">
-                        </b-icon>
-                        <p>Lorem Ipsum ist ein einfacher Demo-Text für die Print- und Schriftindustrie.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
         <hr>
     </section>
 </template>
