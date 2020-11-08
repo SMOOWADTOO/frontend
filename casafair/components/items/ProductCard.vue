dc624222f4cc99770d0557a5b2c9b7e2e171b275
Vi <54704677+beanwillis@users.noreply.github.com>
2020-11-08 22:36:03 +0800

    Updated ProductCard
    Using tag and tooltip to show Product name and description

--- casafair/components/items/ProductCard.vue
+++ casafair/components/items/ProductCard.vue
@@ -1,26 +1,51 @@
 <template>
-    <nuxt-link :to="'/storefront/product/' + productInfo.productId">
+  <nuxt-link :to="'/storefront/product/' + productInfo.productId">
+    <!-- <figure class="image is-1by1">
+      <img :src="productInfo.productPhotoURL" />
+      <p
+        class="is-overlay mx-5 is-size-4-desktop is-size-3-touch"
+        style="margin-top: 60%"
+      >
+        {{ productInfo.productName }}
+        <br />
+        <small class="is-size-6">{{ productInfo.productDesc }}</small>
+      </p>
+    </figure> -->
+
+    <div class="card">
+      <div class="card-image">
         <figure class="image is-1by1">
-            <img :src="productInfo.productPhotoURL">
-            <p class="is-overlay mx-5 is-size-4-desktop is-size-3-touch" style="margin-top: 60%;">
-                {{productInfo.productName}}
-                <br>
-                <small class="is-size-6">{{productInfo.productDesc}}</small>
-            </p>
+          <img :src="productInfo.productPhotoURL" />
         </figure>
-    </nuxt-link>
+      </div>
+      <div class="card-content is-overlay has-text-centered">
+        <b-tooltip
+            type="is-light"
+          position="is-bottom"
+          :label="productInfo.productDesc"
+          multilined
+          animated
+          
+        >
+          <b-tag type="is-primary" size="is-medium" rounded>
+            {{ productInfo.productName }}
+          </b-tag>
+        </b-tooltip>
+      </div>
+    </div>
+  </nuxt-link>
 </template>
 
 <script>
 export default {
-    props: {
-        productInfo: Object,
-    }
-}
+  props: {
+    productInfo: Object,
+  },
+};
 </script>
 
 <style scoped>
-    img {
-        border-radius: 10px !important;
-    }
+img {
+  border-radius: 10px !important;
+}
 </style>
