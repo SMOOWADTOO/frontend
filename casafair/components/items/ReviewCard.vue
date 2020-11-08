59ff702f3caac6380d3ba456a71d70fdd4245249
Chew Jia Rong <jrchew.2018@sis.smu.edu.sg>
2020-11-08 15:11:51 +0800

    Updated Review: Display Review with review card

--- casafair/components/items/ReviewCard.vue
+++ casafair/components/items/ReviewCard.vue
@@ -1,17 +1,28 @@
 <template>
     <div class="box">
         <article class="media">
-            <div class="media-left">
-                <figure class="image is-100x100">
-                    <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
+            <div class="media-left card-content is-flex is-horizontal-center">
+                <figure class="image is-128x128">
+                    <img v-if="product.productPhotoURL == ''" src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
+                    <img v-else :src="product.productPhotoURL">
                 </figure>
             </div>
             <div class="media-content is-vcentered">
                 <div class="content">
                     <p>
-                        <strong>{{rData.name}}</strong> <small>@johnsmith</small> <small>31m</small>
+                        <strong>{{product.productName}}</strong> <small>@{{rData.username}}</small> <small>{{reviewedAt}}</small>
                         <br>
-                        {{rData.review}}
+                        <b-rate
+                            v-model="rData.rating"
+                            :maxs="5"
+                            :show-score="true"
+                            :show-text="true"
+                            :texts="ratingTexts"
+                            :spaced="true"
+                            :disabled="true">
+                        </b-rate>
+                        <br>
+                        {{rData.reviewDetail}}
                     </p>
                 </div>
             </div>
@@ -23,6 +34,63 @@
 export default {
     props: {
         rData: Object,
+    },
+    data() {
+        return {
+            product: {},
+            shop: {}
+        }
+    },
+    mounted() {
+        this.fetchShop(),
+        this.fetchProduct()
+    },
+    computed: {
+        reviewedAt: function() {
+            var publishedAt = this.rData.publishedTime;
+            var pEpoch = Date.parse(publishedAt);
+            var today = new Date();
+            var tEpcoh = Date.parse(today);
+            var diff = tEpcoh - pEpoch;
+            var days = Math.floor(diff / (1000*60*60*24));
+            if (days == 0) {
+                return Math.floor(diff / 60000) + "m";
+                if (minutes > 60) {
+                    return Math.floor(diff/ 1000 * 60 * 60);
+                }
+            }
+            return days + "d";
+        } 
+    },
+    methods: {
+        fetchShop() {
+            let r = this.$axios.get(this.SHOPAPI + "/" + this.rData.shopId)
+            .then((response) => {
+                let objectData = response.data;
+                this.shop = objectData.shop;
+            }).catch((error) => {
+                if (error.response != undefined) {
+                    var response = error.response.data
+                    this.toastAlert(response.message, "is-danger", 5000)
+                } else {
+                    this.toastAlert(error, "is-danger", 5000)
+                }
+            })
+        },
+        fetchProduct() {
+            let r = this.$axios.get(this.PRODUCTAPI + "/" + this.rData.productId)
+            .then((response) => {
+                let objectData = response.data;
+                this.product = objectData.product;
+            }).catch((error) => {
+                if (error.response != undefined) {
+                    var response = error.response.data
+                    this.toastAlert(response.message, "is-danger", 5000)
+                } else {
+                    this.toastAlert(error, "is-danger", 5000)
+                }
+            })
+        }
     }
 }
 </script>
--- casafair/pages/storefront/Review.vue
+++ casafair/pages/storefront/Review.vue
@@ -8,12 +8,37 @@
 export default {
     data() {
         return {
-            reviewData: [{"name": "sadofijsdf", "review": "BAD"}, {"name": "lorem ipsum", "review": "give my money bacl"}]
+            shopID: 0,
+            reviewData: []
         }
+    },
+    created() {
+        this.shopID = parseInt(this.$route.params.id)
+    },
+    mounted() {
+        this.fetchInformation()
     },
     methods: {
         random() {
             return Math.floor(Math.random() * (10 - 1 + 1)) + 10;
+        },
+        fetchInformation() {
+            let r = this.$axios.get(this.REVIEWAPI + "/" + this.shopID)
+            .then((response) => {
+                let objectData = response.data;
+                let reviews = objectData.reviews;
+                for (var review of reviews) {
+                    this.reviewData.push(review);
+                }
+                console.log(this.reviewData);
+            }).catch((error) => {
+                if (error.response != undefined) {
+                    var response = error.response.data
+                    this.toastAlert(response.message, "is-danger", 5000)
+                } else {
+                    this.toastAlert(error, "is-danger", 5000)
+                }
+            })
         }
     }
 }
