eb94f5c096e1ae5db7fa319aabcfa4ccd5c67f82
Vi <54704677+beanwillis@users.noreply.github.com>
2020-11-08 11:09:02 +0800

    Update Contact Us page with Notification API

--- casafair/pages/contact-us/index.vue
+++ casafair/pages/contact-us/index.vue
@@ -6,7 +6,7 @@
             <div class="columns">
                 <div class="column is-6 is-offset-3">
                     <div class="control">
-                        <input class="input" type="text" placeholder="Name">
+                        <input class="input" type="text" v-model="name" placeholder="Name">
                     </div>
                 </div>
             </div>
@@ -15,7 +15,7 @@
             <div class="columns">
                 <div class="column is-6 is-offset-3">
                     <div class="control">
-                        <input class="input" type="email" placeholder="Email">
+                        <input class="input" type="email" v-model="email" placeholder="Email">
                     </div>
                 </div>
             </div>
@@ -24,7 +24,7 @@
             <div class="columns">
                 <div class="column is-6 is-offset-3">
                     <div class="control">
-                        <input class="input" type="text" placeholder="Subject">
+                        <input class="input" type="text" v-model="subject" placeholder="Subject">
                     </div>
                 </div>
             </div>
@@ -32,7 +32,7 @@
         <div class="field">
             <div class="columns">
                 <div class="column is-6 is-offset-3">
-                    <textarea class="textarea" placeholder="Description"></textarea>
+                    <textarea class="textarea" v-model="description" placeholder="Description"></textarea>
                 </div>
             </div>
         </div>
@@ -40,15 +40,73 @@
             <div class="column is-6 is-offset-3">
                 <div class="field is-grouped is-grouped-right">
                     <p class="control">
-                        <a class="button is-primary">
+                        <a class="button is-primary" v-on:click="sendContactUsEmail()">
                             Submit
                         </a>
                     </p>
                     <p class="control">
-                        <a class="button is-light">
+                        <a class="button is-light" v-on:click="cancelContent()">
                             Cancel
                         </a>
                     </p>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="columns my-5">
+            <div class="column is-6 is-offset-3">
+                <p class="title has-text-centered">Helpful topics</p>
+                <br>
+                <nav class="level my-5 px-5">
+                    <div class="level-item has-text-centered">
+                        <div>
+                            <p class="heading">Grievances</p>
+                            <b-icon icon="account" size="is-large" type="is-primary">
+                            </b-icon>
+                        </div>
+                    </div>
+                    <div class="level-item has-text-centered">
+                        <div>
+                            <p class="heading">Claims</p>
+                            <b-icon icon="account" size="is-large" type="is-primary">
+                            </b-icon>
+                        </div>
+                    </div>
+                    <div class="level-item has-text-centered">
+                        <div>
+                            <p class="heading">Feedback</p>
+                            <b-icon icon="account" size="is-large" type="is-primary">
+                            </b-icon>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        <hr>
+        <div class="columns my-5">
+            <div class="column is-6 is-offset-3">
+                <div class="columns is-multiline mx-3">
+                    <div class="column is-6">
+                        <b-icon icon="account" size="is-large" type="is-primary">
+                        </b-icon>
+                        <p>Lorem Ipsum ist ein einfacher Demo-Text für die Print- und Schriftindustrie.</p>
+                    </div>
+                    <div class="column is-6">
+                        <b-icon icon="account" size="is-large" type="is-primary">
+                        </b-icon>
+                        <p>Lorem Ipsum ist ein einfacher Demo-Text für die Print- und Schriftindustrie.</p>
+                    </div>
+                    <div class="column is-6">
+                        <b-icon icon="account" size="is-large" type="is-primary">
+                        </b-icon>
+                        <p>Lorem Ipsum ist ein einfacher Demo-Text für die Print- und Schriftindustrie.</p>
+                    </div>
+                    <div class="column is-6">
+                        <b-icon icon="account" size="is-large" type="is-primary">
+                        </b-icon>
+                        <p>Lorem Ipsum ist ein einfacher Demo-Text für die Print- und Schriftindustrie.</p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -75,6 +133,42 @@
             return {
                 pageName: "Contact Us",
                 pageDescription: "Need any help? Let us know.",
+                name: "",
+                subject: "",
+                email: "",
+                description: "",
+            }
+        },
+        methods: {
+            sendContactUsEmail() {
+                // Create message object
+                var message = {
+                    name: this.name,
+                    email: this.email,
+                    subject: this.subject,
+                    description: this.description
+                }
+                console.log(message)
+                // Call API (/notification/contact-us)
+                let r = this.$axios.post(this.NOTIFICATIONAPI + "/contact-us", message).then((response) => {
+                    let respData = response.data
+                    console.log(respData)
+                    this.toastAlert(respData.message, "is-success", 5000)
+                }).catch((error) => {
+                    if (error.response != undefined) {
+                        var response = error.response.data
+                        this.toastAlert(response.message, "is-danger", 5000)
+                    } else {
+                        this.toastAlert(error, "is-danger", 5000)
+                    }
+                })
+
+            },
+            cancelContent() {
+                this.name = "",
+                this.subject = "",
+                this.email = "",
+                this.description = ""
             }
         }
     }
